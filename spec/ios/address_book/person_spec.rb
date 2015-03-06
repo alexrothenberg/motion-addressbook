f2e6611ad805dff98dcc9130cb82337f04ea0b30
Mark Rickert <mjar81@gmail.com>
2015-03-05 22:48:43 -0500

    Use NSNotificationCenter instead of BubbleWrap.

--- spec/ios/address_book/person_spec.rb
+++ spec/ios/address_book/person_spec.rb
@@ -530,9 +530,9 @@ describe AddressBook::Person do
       @ab1.observe!
       @ab2.observe!
       @notifications = 0
-      App.notification_center.observe :addressbook_updated do |notification|
-        @notifications += 1
-      end
+
+      proc = Proc.new{|notification| @notifications += 1 }
+      NSNotificationCenter.defaultCenter.addObserverForName(:addressbook_updated, object:nil, queue:NSOperationQueue.mainQueue, usingBlock:proc)
     end
 
     # should see a single notification for each change to the AB database:
