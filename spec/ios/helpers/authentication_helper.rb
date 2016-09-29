8bcfc435bcc4ac1ce86ff4e7f9cf65a421aae5f4
Watson <watson1978@gmail.com>
2016-09-29 17:08:16 +0900

    add helper to automatically tap the default button on iOS system dialogs for authentication when run spec

---
+++ spec/ios/helpers/authentication_helper.rb
@@ -0,0 +1,25 @@
+if defined?(UIATarget)
+  # Refer to https://gist.github.com/Voxar/946de66777fc821a63cf5c1d6169504d
+  # Automatically tap the default button on iOS system dialogs for authentication of access to Contacts
+  target = UIATarget.localTarget
+  target.setValue(true, forKey:"handlesAlerts")
+
+  class UIATarget
+    def _handleAlert
+      target = UIATarget.localTarget
+      app = target.frontMostApp
+      alert = app.alert
+      is_system_alert = app._isSystemApplication
+
+      if is_system_alert
+        button = alert.defaultButton
+        if button
+          button.tap
+          return true
+        end
+      end
+      return false
+    end
+  end
+
+end
